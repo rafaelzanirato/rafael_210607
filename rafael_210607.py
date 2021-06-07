823017f2a0e5c0be646426146f3428a042487820
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-06-07 10:59:25 -0300

    Add files via upload

---
+++ rafael_210607.py
@@ -0,0 +1,62 @@
+# Question 2
+fruits=["watermellon","banana","grape","pear","strawberry"]
+# Question 3
+print(fruits[1])
+# Question 4
+fruits.sort()
+# Question 5
+print(fruits)
+# Question 6
+fruits.append("apple")
+# Question 7
+print(fruits)
+# Question 8
+fruits.insert(0,"zucchini")
+# Question 9
+print(fruits)
+# Question 10
+froits=(sorted(fruits))
+# Question 11
+print(fruits)
+print(froits)
+# Question 12
+fruits.append("tamato")
+# Question 13
+print(fruits)
+print(froits)
+# Question 14 
+list=["adidas","nike","puma"]
+# Question 15
+print(list)
+# Question 16
+for item in list:
+    print(item)
+# Question 17
+for item in list:
+    print(item)
+    print(item)
+# Question 18
+for item in list:
+    print(item.upper())
+# Question 19
+print("This is my list:")
+for item in list:
+    print(item)
+# Question 20
+for item in list:
+    print(item)
+print("This was my list.")
+# Question 21
+print("This is one item of my list:")
+print(list[0])
+print("This is one item of my list:")
+print(list[1])
+print("This is one item of my list:")
+print(list[2])
+# Question 22
+print(list[0])
+print("This was one item of my list.")
+print(list[1])
+print("This was one item of my list.")
+print(list[2])
+print("This was one item of my list.")
